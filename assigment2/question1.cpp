aa44d1294820533cd7c50d42308ca9cf4f805480
Nadeem Esrar <nadeemesrar020@gmail.com>
2025-08-19 11:30:53 +0530

    1024030291

---
+++ assigment2/question1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key) {
+            return mid; // found
+        }
+        else if (arr[mid] < key) {
+            low = mid + 1; // search right half
+        }
+        else {
+            high = mid - 1; // search left half
+        }
+    }
+    return -1; // not found
+}
+
+int main() {
+    int arr[100], n, key;
+
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter elements in sorted order:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
